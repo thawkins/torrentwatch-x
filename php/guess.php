599eec09fe1f011576ea1d0425aca72c1271c108
Joris Vandalon <torrentwatch-x@wiebel.nl>
2010-01-26 18:49:05 +0100

    argh....

--- php/guess.php
+++ php/guess.php
@@ -84,6 +84,7 @@ function guess_match($title, $normalize = FALSE) {
     if(preg_match('/^x\d+/', $episode_guess)) {
         $episode_guess = preg_replace('/(^x)(\d+)/', '1x\2', $episode_guess);
     }
+    $episode_guess = preg_replace('/0*(\d+)x0*(\d+)/', '\1x\2', $episode_guess);
     if(preg_match('/^(\d\d\d\d \d\d \d\d)$/', $episode_guess)) {
         $episode_guess = preg_replace('/ /', '', $episode_guess);
     }
