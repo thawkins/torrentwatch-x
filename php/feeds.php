8a6215e3d5f835b929766396115ab97a77b01c9d
Joris Vandalon <torrentwatch-x@wiebel.nl>
2010-03-23 10:12:16 +0100

    Wait 1 sec after every hit

--- php/feeds.php
+++ php/feeds.php
@@ -172,16 +172,15 @@ function check_for_torrent(&$item, $key, $opts) {
       if($link = get_torrent_link($rs)) {
         if(client_add_torrent($link, NULL, $rs['title'], $opts['URL'], $item)) {
             add_cache($rs['title']);
-            sleep(1);
         } else {
             _debug("Failed adding torrent $link\n", -1);
             return FALSE;
         }
-
       } else {                     
         _debug("Unable to find URL for ".$rs['title']."\n", -1);
         $matched = "nourl";
       }
+      sleep(1);
     }
   }
 }
