9230018fb55cfaa0e1e003eaa86bb227bb3769cf
Joris Vandalon <torrentwatch-x@wiebel.nl>
2010-03-24 16:04:17 +0100

    Fixed stupid bug wich would cause configfile to be overwritten

--- NMT/daemon.sh
+++ NMT/daemon.sh
@@ -5,8 +5,11 @@ install()
 		if [ ! -f /share/Apps/Torrentwatchx/php/config.php ] ; then 
 			cp /share/Apps/Torrentwatchx/php/config.php.dist /share/Apps/Torrentwatchx/php/config.php
 		fi
+		if [ ! -f /share/Apps/Torrentwatchx/php/config.php ] ; then 
+			cp /share/Apps/Torrentwatchx/etc/torrentwatch.config.dist /share/Apps/Torrentwatchx/etc/torrentwatch.config
+		fi
                 chmod 777 /share/Apps/Torrentwatchx/etc
-                chmod 777 /share/Apps/Torrentwatchx/etc/torrentwatch.config
+                chmod 666 /share/Apps/Torrentwatchx/etc/torrentwatch.config
                 chmod 777 /share/Apps/Torrentwatchx/rss_cache
 }
 
