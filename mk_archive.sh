d5d87ebcf0a0208065708cbe84287eb03c0c66c8
Joris Vandalon <torrentwatch-x@wiebel.nl>
2010-11-02 18:51:38 +0100

    mkarchive.sh fix and after-release changes

--- mk_archive.sh
+++ mk_archive.sh
@@ -7,7 +7,7 @@ hg archive -r $1 -X '.hg*' -X 'mk_archive.sh' -X 'robots.txt' -X 'php/config.php
 mkdir -p TorrentWatchX-$1/docs
 for i in wiki/*.wiki ; do cp $i TorrentWatchX-$1/docs/ ; done
 tar uf /var/www/torrentwatch-x/releases/TorrentWatchX-$1.tar TorrentWatchX-$1/docs/
-rm -rf TorrentWAtchX-$1/ Torrentwatchx/
+rm -rf TorrentWAtchX-$1/ 
 gzip /var/www/torrentwatch-x/releases/TorrentWatchX-$1.tar
 
 #NMT Package
@@ -34,7 +34,8 @@ cd Torrentwatchx/
 sudo tar upf /var/www/torrentwatch-x/releases/Torrentwatchx.tar .
 cd ..
 rm -rf TorrentWatchX-$1/
+rm -rf Torrentwatchx/
 cd releases
 zip TorrentWatchX-NMT-$1.zip Torrentwatchx.tar
-rm Torrentwatchx.tar
+rm -f Torrentwatchx.tar
 
