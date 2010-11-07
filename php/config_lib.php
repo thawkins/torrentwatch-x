7b997e79c110466dfcc5da2db324bcc7cd9cb4e2
Joris Vandalon <torrentwatch-x@wiebel.nl>
2010-11-07 01:35:24 +0100

    Fixed an issue where in some cases Last epi info would not be updated

--- php/config_lib.php
+++ php/config_lib.php
@@ -445,7 +445,7 @@ function update_feedData() {
 	foreach ($config_values['Favorites'] as &$favorite) {
 	  if ($favorite['Feed'] == $old_feedurl) $favorite['Feed'] = preg_replace('/ /', '%20', $_GET['feed_link']);
 	}
-
+	
         $config_values['Feeds'][$_GET['idx']]['Name'] = $_GET['feed_name'];
         $config_values['Feeds'][$_GET['idx']]['Link'] = preg_replace('/ /', '%20', $_GET['feed_link']);
         $config_values['Feeds'][$_GET['idx']]['seedRatio'] = $_GET['seed_ratio'];
