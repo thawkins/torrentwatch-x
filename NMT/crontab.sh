8b52fdb937f55dcd7e96d1711f00e457380efd1c
Joris Vandalon <torrentwatch-x@wiebel.nl>
2010-12-04 17:44:08 +0100

    fixed issue where https feeds would fail on NMT

--- NMT/crontab.sh
+++ NMT/crontab.sh
@@ -16,5 +16,9 @@ then
 PHPCGI="/mnt/syb8634/server/php5-cgi"
 fi
 
+if [ -e "/mnt/syb8634/server/php5-cgi" ];
+then
+PHPCGI="/share/Apps/lighttpd/bin/php-cgi";
+fi
+
 ${PHPCGI} -q rss_dl.php -D >/dev/null 2>&1
-${PHPCGI} -q transmission_queue.php >/dev/null 2>&1
