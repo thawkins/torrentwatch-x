1bcb5acb3446a3fb85295d4bfaac1d47640434ce
Joris Vandalon <torrentwatch-x@wiebel.nl>
2010-12-04 19:28:42 +0100

    merge

--- php/tor_client.php
+++ php/tor_client.php
@@ -212,7 +212,7 @@ function client_add_torrent($filename, $dest, $title, $feed = NULL, &$fav = NULL
   }
   $getOptions[CURLOPT_URL] = $url;
   if(isset($cookies)) $getOptions[CURLOPT_COOKIE] = $cookies;
-  $getOptions[CURLOPT_USERAGENT] = 'Python-urllib/1.17';  
+  //$getOptions[CURLOPT_USERAGENT] = 'Python-urllib/1.17';  
   $getOptions[CURLOPT_FOLLOWLOCATION] = true;
   get_curl_defaults($getOptions);
   curl_setopt_array($get, $getOptions);
