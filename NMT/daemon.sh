49c6c2e8d9fc4331b4f0b74e12eee9d5d29cc616
Joris Vandalon <torrentwatch-x@wiebel.nl>
2011-11-15 16:35:43 +0100

    updated installer

--- NMT/daemon.sh
+++ NMT/daemon.sh
@@ -23,6 +23,8 @@ install()
 		if [ -d javascript ] ; then rm -rf javascript/ ; fi
 		if [ -d images ] ; then rm -rf images/ ; fi
 		if [ -e info.php ] ; then rm info.php ; fi
+		if [ -e torrentwatch.php ] ; then rm torrentwatch.php ; fi
+		if [ -e index.html ] ; then rm index.html ; fi
 		if [ -e TWXRepository.xml ] ; then rm TWXRepository.xml ; fi
 		if [ -e Release-Checklist ] ; then rm Release-Checklist ; fi
 		if [ -e etc/torrentwatch.config.dist ] ; then rm etc/torrentwatch.config.dist ; fi
