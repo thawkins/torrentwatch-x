a2d1e7d76b7ea2c3aee2b366dfe382f7be4d0d30
Joris Vandalon <torrentwatch-x@wiebel.nl>
2012-03-13 11:31:22 +0100

    finetuning sanatizing of hidelist

--- web/update_hidelist.php
+++ web/update_hidelist.php
@@ -1,12 +1,12 @@
 <?php
-ini_set('include_path', '.:./web/php');
+ini_set('include_path', '.:./php');
 require_once('rss_dl_utils.php');
 
 read_config_file();
 
 foreach($config_values['Hidden'] as $key => $hidden) {
 	unset($config_values['Hidden'][$key]);
-	$config_values['Hidden'][strtolower($key)] = "hidden";
+	$config_values['Hidden'][strtolower(strtr($key, array(":" => "", "," => "", "'" => "", "." => " ",  "_" => " ")))] = "hidden";
 }
 
 write_config_file();
